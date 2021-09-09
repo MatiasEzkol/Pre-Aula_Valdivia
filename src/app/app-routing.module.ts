a0f24a7894fc929550443383d7c3d51447621ff2
Matias <matiasmurafarias@gmail.com>
2021-09-09 15:20:34 -0300

    login and dashboard

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { LoginComponent } from './page/login/login.component';
+
+const routes: Routes = [
+  {
+    path:"login",
+    component: LoginComponent
+  },
+  {
+    path:"dashboard",
+    component: DashboardComponent
+  },
+  {
+    path:"**",
+    redirectTo: "login"
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
